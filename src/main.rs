45867a63a9edb45a6b1aa2f5731f3967cd9bbae2
nwzy <23061078+nwzy@users.noreply.github.com>
2026-01-27 14:42:23 -0800

    Simulate RESP protocol workload time

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ use tokio::net::{TcpListener, TcpStream};
 
 async fn handle_connection(socket: TcpStream, addr: SocketAddr) {
     println!("Processing connection from {}", addr);
+
+    // Simulation of RESP protocol parsing
+    tokio::time::sleep(
+        tokio::time::Duration::from_secs(5), // );
+    )
+    .await;
+
+    println!("Finished processing {}", addr);
 }
 
 #[tokio::main]
